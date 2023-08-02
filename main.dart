433a32217d3ed93bf5fabc16583bb3616c58e5f9
mayank <prajapatmayank2905@gmail.com>
2023-08-02 10:17:32 +0530

    data types

---
+++ main.dart
@@ -0,0 +1,17 @@
+void main() {
+  // String newString = '100';
+  // double value3 = 1;
+  // int value = 10;
+  // print(value);
+  // String ab = 'herg';
+  // dynamic dValue = 'hello';
+  bool bValue = false;
+  print(bValue);
+  // print(ab.toUpperCase());
+
+  // dValue = 10;
+  // value = 5;
+  // var value2;
+
+  // print(dValue.runtimeType);
+}
